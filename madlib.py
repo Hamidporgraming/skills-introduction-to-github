e83641ce985f27fa900e296e0b656e42a4d10a1b
Hamidporgraming <mimou-real@hotmail.com>
2024-12-03 20:10:43 +0100

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,9 @@
+#MADLIB
+noun= input("noun: ")
+adj= input("adj: ")
+noun1= input("noun1: ")
+noun2= input("noun2: ")
+madlib= (f"best frined is a person who you {noun} above other "
+          f"frineds in your life someone you have {adj} with"
+          f",someone you {noun1} and someone in whom you {noun2}")
+print(madlib)
